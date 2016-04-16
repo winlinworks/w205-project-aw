356b78a39338e113660018d3f82bae1df97c4792
winlingit <winlin@gmail.com>
2016-04-15 21:34:26 -0500

    updated python ftp script

--- test/extract.py
+++ test/extract.py
@@ -1,5 +1,10 @@
 import sys
-from ftplib import ftplib
+from ftplib import FTP
 
 ftp = FTP('ftp.fec.gov')	# connect to host: ftp.fec.gov
 ftp.login()		# user = anonymous, pass = anonymous
+
+ftp.cwd('FEC')	# change directory to 'FEC'
+ftp.retrlines('LIST')	# list directory contents
+
+ftp.quit()
