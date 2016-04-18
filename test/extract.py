ebb1dbb59621fb455bac7b8db7a1915d15f2ff11
winlingit <winlin@gmail.com>
2016-04-18 13:40:42 -0500

    added dictionaries for files

--- test/extract.py
+++ test/extract.py
@@ -6,44 +6,46 @@ import ftplib
 from ftplib import FTP
 
 
-CURR_DIR = os.getcwd()              # parent directory where main apps are
-DEST_DIR = CURR_DIR + '/data_raw/'  # directory where files will be stored
+PROJ_DIR = os.getcwd()              # parent directory where main apps are
+RAW_DIR = PROJ_DIR + '/data_raw/'  # directory where files will be stored
 HOST = 'ftp.fec.gov'
 
 
-# dictionary for election years and associated file names
-FILE_DICT = {'2016': ['cm16.zip', 'cn16.zip', 'pas216.zip', 'indiv16.zip'],
+# dictionary for election years and downloaded file names
+FILE_DICT = {
+            '2016': ['cm16.zip', 'cn16.zip', 'pas216.zip', 'indiv16.zip'],
             '2014': ['cm14.zip', 'cn14.zip', 'pas214.zip', 'indiv14.zip'],
-            '2012': ['cm12.zip', 'cn12.zip', 'pas212.zip', 'indiv12.zip'],
-            'headers': ['cm_header_file.csv', 'cn_header_file.csv', 'pas2_header_file.csv', 'indiv_header_file.csv']}
+            '2012': ['cm12.zip', 'cn12.zip', 'pas212.zip', 'indiv12.zip']
+            }
 
-# file names for unzipped files, same across all years
+# file names for header files, same across all years
+HEADER_FILES = ['cm_header_file.csv', 'cn_header_file.csv', 'pas2_header_file.csv', 'indiv_header_file.csv']
+
+# file names for unzipped text files, same across all years
 UNZIP_FILES = ['cm.txt', 'cn.txt', 'itcont.txt', 'pas2.txt']
 
 
-# downloads all raw zip files for contributions, candidates, and committees from ftp.fec.gov
+# downloads raw data for contributions, candidates, and committees from ftp.fec.gov
 def getFiles():
     try:
-        os.chdir(DEST_DIR)          # change working directory
-
         ftp = FTP('ftp.fec.gov')    # connect to host: ftp.fec.gov
         ftp.login()                 # user = anonymous, pass = anonymous
 
-        # download main files
-        for key in FILE_DICT:
-            ftp.cwd('/FEC/' + key)      # move to matching FTP directory
-            ftp.retrlines('LIST')       # list files
+        os.chdir(RAW_DIR)           # move to raw data directory
 
-            filenames = FILE_DICT[key]  # get file names
+        # download files for each year
+        for k, v in FILE_DICT.iteritems():
+            ftp.cwd('/FEC/' + k)      # move to FTP directory matching year
+            ftp.retrlines('LIST')     # list files
 
-            # for each file name, download file from FTP site
-            for fn in filenames:
+            # download files
+            for fn in v:
                 f = open(fn, 'wb')
                 ftp.retrbinary('RETR ' + fn, f.write)
                 f.close()
 
         # download header files
-        for fn in FILE_DICT['headers']:
+        for fn in HEADER_FILES:
             url = 'http://www.fec.com/finance/disclosure/metadata/' + fn
             urllib.urlretrieve(url, fn)
 
@@ -52,36 +54,34 @@ def getFiles():
         os.unlink(fn)
 
     ftp.quit()
-    os.chdir(CURR_DIR)      # return to main project directory
+    os.chdir(PROJ_DIR)      # return to project directory
 
 
 # unzips all raw zip files to text files, adds year suffix to text file name
 def unzipFiles():
-    os.chdir(DEST_DIR)          # change working directory
+    os.chdir(RAW_DIR)          # move to raw data directory
 
     # for each year
-    for key in FILE_DICT:
-        suffix = key[2:]        # suffix for year
-
-        filenames = FILE_DICT[key]  # get file names
+    for k, v in FILE_DICT.iteritems():
+        suffix = k[2:]        # suffix for year
 
         # unzip each file
-        for fn in filenames:
+        for fn in v:
             f = open(fn, 'rb')
             zf = zipfile.ZipFile(f)
             zf.extractall()
             zf.close()
 
         # add year suffix to each text file
-        for fn in os.listdir(DEST_DIR):         
+        for fn in os.listdir(RAW_DIR):         
             if fn in UNZIP_FILES:
                 currfile = fn
                 newfile = fn[:-4] + suffix + '.txt'
                 os.rename(currfile, newfile)    # rename file
 
-    os.chdir(CURR_DIR)      # return to main project directory
+    os.chdir(PROJ_DIR)      # return to project directory
 
 
 if __name__ == '__main__':
-    # getFiles()
+    getFiles()
     unzipFiles()
--- test/transform.py
+++ test/transform.py
@@ -1,24 +1,61 @@
 import sys, os
 import pandas as pd
 
+from googleapiclient.discovery import build
+from googleapiclient.errors import HttpError
+from oauth2client.client import GoogleCredentials
+
 
 CURR_DIR = os.getcwd()              # parent directory where main apps are
-DEST_DIR = CURR_DIR + '/data_raw/'  # directory where files will be stored
+RAW_DIR = CURR_DIR + '/data_raw/'  # directory where files will be stored
+NEW_DIR = CURR_DIR + '/data_new/'  # directory where files will be stored
 
 FILE_STEMS = ['cm', 'cn', 'itcont', 'itpas2']
 
+CODE_FIELDS = ['amndt_ind', 'cand_ici', 'cand_status', 'cmte_dsgn', 'cmte_filing_freq', 'cmte_tp', 'entity_tp', 'org_tp', 'office', 'rpt_tp', 'transaction_pgi', 'transaction_tp']
+CODE_DICT = {''}
+
+
+# APPROACH
+
+# Method 1: normalize/join data locally, then load to BQ
+# - Use pandas for more intuitive workflow
+# - Filter future data before loading to reduce size of loads
+# - Can merge on 2 cols/indices
+
+# Method 2: load all raw/denormalized data to BQ, then normalize/join data in BQ
+
+
+# IMPLEMENTATION
+
+# With dataframs df1, df2
+# - Merge: pd.merge(df1, df2, on='field', how='left')
+# - Join: df1.set_index('field', inplace=True); df1.join(df2)
 
 # Candidate Master (cmXX.txt)
 # 1. normalize cols: CAND_OFFICE, CAND_ICI, CAND_STATUS, CAND_PTY_AFFILIATION
 # 2. delete cols: CAND_ST1, CAND_ST2, CAND_CITY, CAND_ST, CAND_ZIP
 # 3. filter rows: CAND_OFFICE = P, CAND_PTY_AFFILIATION = {DEM, REP, IND}
 
+def getCodes():
+    for fn in os.listdir(DEST_DIR):
+        if fn.startswith('cd_'):
+            codename = fn[2:-4]
+
+
 def transformCandidates():
-    # read csv into dataframe
+    # read csv into dataframe, set headers, set index
+    df = pd.read_csv(filename, names=getHeaders(), index_col=0)
+
+    codes = getCodes()      # get dictionary of codes
+    pd.merge(df, currcode, on# merge dataframe with codes
 
     # create new dataframe with selected cols
 
     # filter rows to relevant elected offices and parties
+
+    # save csv to new data directory
+    df.to_csv(newfilename, header = False)
     return
 
 
