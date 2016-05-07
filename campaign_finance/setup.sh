b443bbe258a3e14e78d56651b831c6540e34f3f6
winlingit <winlin@gmail.com>
2016-05-07 14:00:18 -0500

    updated setup script

--- campaign_finance/setup.sh
+++ campaign_finance/setup.sh
@@ -1,11 +1,20 @@
-sudo yum install python27-devel -y
-sudo mv /usr/bin/python /usr/bin/python266
-sudo ln -s /usr/bin/python2.7 /usr/bin/python
+yum install gcc
+cd /usr/src
+wget https://www.python.org/ftp/python/2.7.11/Python-2.7.11.tgz
+tar xzf Python-2.7.11.tgz
+cd Python-2.7.11
+./configure
+make altinstall
+mv /usr/bin/python /usr/bin/python266
+ln -s /usr/bin/Python-2.7.11/python /usr/bin/python
+python -V
 
 sudo curl -o ez_setup.py https://bootstrap.pypa.io/ez_setup.py
 sudo python ez_setup.py
 sudo /usr/bin/easy_install-2.7 pip
 sudo pip install virtualenv
+
+virtualenv -p /usr/bin/python2.7 venv
 
 mkdir data_raw
 mkdir data_master
